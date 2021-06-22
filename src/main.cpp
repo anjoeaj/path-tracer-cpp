35204b108f678f08a1d95e9295357e18d45fe08f
Anjoe Jacob <anjoeaj@gmail.com>
2021-06-23 00:32:35 +0100

    anti-aliasing added based on spp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
-
+#include "camera.h"
 
 #include <iostream>
 
@@ -38,6 +38,7 @@ int main(){
 	const auto aspect_ratio = 16.0/9.0;
 	const int width = 400;
 	const int height = static_cast<int> (width/aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// Create the world
 	hittable_list world;
@@ -45,15 +46,7 @@ int main(){
 	world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
 	//Camera details
-	// the viewport is essentially the projection plane
-	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * aspect_ratio;
-	auto focal_length = 1.0; // the length from camera to the projection plane
-
-	auto origin = point3(0,0,0);
-	auto horizontal = vec3(viewport_width,0,0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
+	camera cam;
 	
 	// Now create a PPM format for storing the image data.
 	// Check wikipedia for the format
@@ -63,13 +56,15 @@ int main(){
 	for (int j = height - 1; j >= 0; j--){
 		std::cerr << "\rScanlines remaining: "<<j << " " <<std::flush;
 		for (int i = 0; i < width ; i ++){
-			double u = (double)i/(width - 1);
-			double v = (double)j/(height - 1);
+			color pixel_color = color(0,0,0);
+			for (int s = 0; s < samples_per_pixel; s++){
+				double u = (i + random_double()) / (width - 1);
+				double v = (j + random_double()) / (height - 1);
 
-
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+			}
+		write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr <<"\nDone";
