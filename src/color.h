e164e36a06c470cceb0675fb41b84619dd5ba318
Anjoe Jacob <anjoeaj@gmail.com>
2021-06-23 12:09:28 +0100

    gamma correction

--- src/color.h
+++ src/color.h
@@ -5,9 +5,19 @@
 
 void write_color(std::ostream &out, color pixel_color, double samples_per_pixel){
 	pixel_color = pixel_color/samples_per_pixel;
-	auto r = clamp(pixel_color.r(), 0.0, 0.999);
-	auto g = clamp(pixel_color.g(), 0.0, 0.999);
-	auto b = clamp(pixel_color.b(), 0.0, 0.999);
+	auto r = pixel_color.r();
+	auto g = pixel_color.g();
+	auto b = pixel_color.b();
+
+	// gamma correct the pixels. Use gamma 2
+	r = sqrt(r);
+	g = sqrt(g);
+	b = sqrt(b);
+
+	// clamp between [0,1)
+	r = clamp(r, 0.0, 0.999);
+	g = clamp(g, 0.0, 0.999);
+	b = clamp(b, 0.0, 0.999);
 	// cas the float to int and write to the stream
 	out << static_cast<int>(255.999 * r) << " "
 		<< static_cast<int>(255.999 * g) << " "
